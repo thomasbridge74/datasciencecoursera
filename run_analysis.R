6f98c9c9d2a730ec97338b31291835628ca0e145
Thomas Bridge <tbridge@chicagobooth.edu>
2014-04-26 15:56:13 +0100

    First commit

---
+++ run_analysis.R
@@ -0,0 +1,19 @@
+# This script runs the commands to merge the two data sets.
+
+# The assumption is we're in the root folder of the data set.
+
+
+
+# 1. Merges the training and the test sets to create one data set.
+
+# 2. Extracts only the measurements on the mean and standard deviation for 
+#   each measurement. 
+
+# 3. Uses descriptive activity names to name the activities in the data set
+
+# 4. Appropriately labels the data set with descriptive activity names. 
+
+# 5. Creates a second, independent tidy data set with the average of each 
+#    variable for each activity and each subject. 
+
+
