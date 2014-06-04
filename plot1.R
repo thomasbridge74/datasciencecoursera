d8e3b5c2e9661136826a577d3085c996821e1c1c
Thomas Bridge <tbridge@chicagobooth.edu>
2014-06-04 12:20:36 +0100

    Initial addition of plot1.R - PNG not done

---
+++ plot1.R
@@ -0,0 +1,4 @@
+electricity <- read.table("household_power_consumption.txt",sep=";", header=TRUE, colClasses = c("factor", "factor", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), na.strings = "?")
+electricity$Date <- as.Date(electricity$Date, format = "%d/%m/%Y")
+sub_elec <- electricity[electricity$Date ==  "2007-02-01" | electricity$Date == "2007-02-02", ]
+hist(sub_elec$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power")
