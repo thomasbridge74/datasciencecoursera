5ab2577c0365fd413d1bcb4cf1a4ef154c634a96
Thomas Bridge <tbridge@chicagobooth.edu>
2014-06-04 16:23:29 +0100

    Added PNG output

--- plot1.R
+++ plot1.R
@@ -1,4 +1,13 @@
+
+# The first three lines of this R file load the file and selects the 
+# required subsets of dates.
+
 electricity <- read.table("household_power_consumption.txt",sep=";", header=TRUE, colClasses = c("factor", "factor", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), na.strings = "?")
 electricity$Date <- as.Date(electricity$Date, format = "%d/%m/%Y")
 sub_elec <- electricity[electricity$Date ==  "2007-02-01" | electricity$Date == "2007-02-02", ]
+
+# Now, produce the diagram.
+
+png("plot1.png")
 hist(sub_elec$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power")
+dev.off()
